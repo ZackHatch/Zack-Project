281d03d658ea90abc0201bb6bb89110ce3e2094d
Zachary B Hatch <zh86549@srcs.loc>
2017-12-13 12:12:24 -0800

    new

--- final_challenge_functions.js
+++ final_challenge_functions.js
@@ -1,9 +1,9 @@
 // Prime Number Fucntion
 function checkPrime(number) {
-    if (number < 4) {
-        return "Prime"
-    } else if (number < 2 || number % 1 != 0 || number == 0) {
+    if (number < 2 || number % 1 != 0 || number == 0) {
         return "Not a Valid Input"
+    } else if (number < 4) {
+        return "Prime"
     } else(number % 1 == 0)
     for (i = 2; i <= number / 2; i++) {
         if (number % i == 0) {
