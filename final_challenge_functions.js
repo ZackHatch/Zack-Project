1945ff9bbc2bdd2df403d8f63b637f2f003159b3
Zachary B Hatch <zh86549@srcs.loc>
2017-12-15 11:19:48 -0800

    uyg u

--- final_challenge_functions.js
+++ final_challenge_functions.js
@@ -15,4 +15,28 @@ function checkPrime(number) {
     return "Prime"
 }
 
-//
+//Times Table
+function timesTable(number){
+    var rows = number;
+    var cells = number;
+    document.write("<table>");
+    for (i = 1; i <= rows; i++){
+        document.write("<tr>");
+        for(j = 1; j <= cells; j++){
+            document.write("<td>", i * j, "<td>");
+        }
+    document.write("</tr>");
+    }
+    return ("</table>")
+}
+
+//Shuffle thing
+function shuffle (someArray) {
+    for ( var i = someArray.length - 1; i > 0; i--) {
+         var j = Math.floor(Math.random() * (i + 1));
+         var x = someArray[i];
+        someArray[i] = someArray[j];
+        someArray[j] = x;
+    }
+    return someArray;
+}
